035428c4f93b4f4a3c315ca341134a0b80164ff8
Jeronimo <jeronimo.irazabal@gmail.com>
2024-03-11 11:06:48 -0300

    hashtree: improved error handling
    Signed-off-by: Jeronimo <jeronimo.irazabal@gmail.com>

--- adapters/repos/db/shard.go
+++ adapters/repos/db/shard.go
@@ -729,9 +729,12 @@ func (s *Shard) initHashTree(ctx context.Context) error {
 	}
 
 	if partitioningEnabled {
-		s.buildCompactHashTree()
+		s.hashtree, err = s.buildCompactHashTree()
 	} else {
-		s.buildMultiSegmentHashTree()
+		s.hashtree, err = s.buildMultiSegmentHashTree()
+	}
+	if err != nil {
+		return err
 	}
 
 	// sync hashtree with current object states
@@ -822,11 +825,11 @@ func (s *Shard) UpdateAsyncReplication(ctx context.Context, enabled bool) error 
 	return nil
 }
 
-func (s *Shard) buildCompactHashTree() {
-	s.hashtree = hashtree.NewCompactHashTree(math.MaxUint64, 16)
+func (s *Shard) buildCompactHashTree() (hashtree.AggregatedHashTree, error) {
+	return hashtree.NewCompactHashTree(math.MaxUint64, 16)
 }
 
-func (s *Shard) buildMultiSegmentHashTree() {
+func (s *Shard) buildMultiSegmentHashTree() (hashtree.AggregatedHashTree, error) {
 	shardState := s.index.shardState
 
 	virtualNodes := make([]sharding.Virtual, len(shardState.Virtual))
@@ -862,7 +865,7 @@ func (s *Shard) buildMultiSegmentHashTree() {
 		segments[i] = hashtree.NewSegment(segmentStart, segmentSize)
 	}
 
-	s.hashtree = hashtree.NewMultiSegmentHashTree(segments, 16)
+	return hashtree.NewMultiSegmentHashTree(segments, 16)
 }
 
 func (s *Shard) closeHashTree() error {
